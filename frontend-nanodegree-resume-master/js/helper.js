88f8c7d280ce70f795c66d8a2d3f320879211ec8
jg4672 <36344796+jg4672@users.noreply.github.com>
2018-02-14 16:49:13 -0800

    Class Role was added to HTMLheaderRole
    Class Role was added to HTMLheaderRole due to CSS style change

--- frontend-nanodegree-resume-master/js/helper.js
+++ frontend-nanodegree-resume-master/js/helper.js
@@ -13,7 +13,8 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+//Added class role for CSS custimation
+var HTMLheaderRole = '<span class="role">%data%</span><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
